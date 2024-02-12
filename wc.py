feae4580c52558a0ed83b6983636eaea18c68713
Aniket Patanwal <aniket.patanwal99@gmail.com>
2024-02-12 21:49:39 +0530

    -m and default

--- wc.py
+++ wc.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 import traceback
+import locale
 
 def get_stdout(res, files):
     out = ""
@@ -16,7 +17,13 @@ def get_stdout(res, files):
         out += f'{total[i]} '
     out += 'total\n'
     return out
-    
+
+def default_action(args):
+    if not any([args.c, args.m, args.l, args.w]):
+        args.c = True
+        args.w = True
+        args.l = True
+
 def main(args):
     try:
         files = args.file if isinstance(args.file, list) else [args.file]
@@ -31,15 +38,22 @@ def main(args):
                 with open(file_path, 'r') as fp:
                     flines = len(fp.readlines())
                 r.append(flines)
-            if args.c:
-                fsz = os.path.getsize(file_path)
-                r.append(fsz)
             if args.w:
                 words = 0
                 with open(file_path, 'r') as fp:
                     d = fp.read()
                     words += len(d.split())
                 r.append(words)
+            if args.c:
+                fsz = os.path.getsize(file_path)
+                r.append(fsz)
+            if args.m:
+                encoding = locale.getpreferredencoding()
+                chars = 0
+                with open(file_path, encoding=encoding) as fp:
+                    for line in fp:
+                        chars += len(line)
+                r.append(chars)
             res.append(r)
         sys.stdout.write(get_stdout(res, files))
     except Exception as ex:
@@ -64,6 +78,14 @@ if __name__ == '__main__':
         help="The number of words in each input file is written to the standard output.",
         action="store_true"
     )
+    parser.add_argument(
+        "-m",
+        help="The number of characters in each input file is written to the standard output.  If the current locale does not support multibyte characters, this is equivalent to the -c option.  This will\
+             cancel out any prior usage of the -c option.",
+        action="store_true"
+    )
     parser.add_argument("file", help="Path to the file to be processed", nargs='+', type=str)
+    parser.set_defaults(func=default_action)
     args = parser.parse_args()
+    args.func(args)
     main(args)
